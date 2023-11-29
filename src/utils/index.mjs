6486ab0f96a56c148116d1d4f3c257e6c1078186
Juan Pablo <juan.ramirez.q@gmail.com>
2023-11-29 16:51:43 -0500

    fix export in utils

--- src/utils/index.mjs
+++ src/utils/index.mjs
@@ -14,7 +14,7 @@ export function loadInput(dirPath, fileName, separator = '\n') {
   return result.split(separator);
 };
 
-const print2DMatrix = (matrix) => {
+export function print2DMatrix(matrix) {
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[0].length; j++) {
       process.stdout.write(matrix[i][j].toString());
