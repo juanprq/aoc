4dc3480fd1d36e261c106ea58b41b4718de885aa
Juan Pablo <juan.ramirez.q@gmail.com>
2023-12-01 16:39:43 -0500

    adds 2020 day 15

---
+++ src/2020/day-15/problem-2.mjs
@@ -0,0 +1,32 @@
+export default function(input) {
+  const data = input[0]
+    .split(',')
+    .map(v => parseInt(v, 10));
+
+  const memo = input.slice(0, input.length - 1).reduce((accum, number, idx) => {
+    accum[number] = idx + 1;
+    return accum;
+  }, new Array(30e6));
+
+  let start = new Date();
+  let lastNumber = input[input.length -1];
+  for (let i = input.length; i < 30e6; i++) {
+    if (memo[lastNumber]) {
+      const newNumber = i - memo[lastNumber];
+      memo[lastNumber] = i;
+      lastNumber = newNumber;
+    } else {
+      memo[lastNumber] = i;
+      lastNumber = 0;
+    }
+
+    if (i % 1e6 === 0) {
+      const end = new Date() - start
+      start = new Date();
+    }
+  }
+
+  return lastNumber;
+}
+
+export const expectedValue = 436;
