8294a3614232a078928d79c228ca4ea8e2c3940f
Juan Pablo <juan.ramirez.q@gmail.com>
2024-12-05 08:42:19 -0500

    adds 2024 day 5

---
+++ src/2024/day-5/problem-1.mjs
@@ -0,0 +1,56 @@
+export default function(input) {
+  const rulesMap = new Map();
+  const pages = [];
+  let toPages = false;
+
+  for (let row of input) {
+    if (row === '') {
+      toPages = true;
+      continue;
+    }
+    if (toPages) {
+      pages.push(row.split(',').map(value => parseInt(value, 10)));
+      continue;
+    }
+
+    const [a, b] = row.split('|').map(value => parseInt(value, 10));
+    if (!rulesMap.has(a)) {
+      rulesMap.set(a, new Set());
+    }
+
+    rulesMap.get(a).add(b);
+  }
+
+  let result = 0;
+
+  for (let page of pages) {
+    let valid = true;
+
+    for (let i = 0; i < page.length - 1; i++) {
+      const a = page[i];
+
+      for (let j = i + 1; j < page.length; j++) {
+        const b = page[j];
+
+        if (!rulesMap.has(a)) {
+          valid = false;
+          break;
+        }
+        if (!rulesMap.get(a).has(b)) {
+          valid = false;
+          break;
+        }
+      }
+
+      if (!valid) break;
+    }
+
+    if (valid) {
+      result += page[page.length >> 1];
+    }
+  }
+
+  return result;
+}
+
+export const expectedValue = 143;
